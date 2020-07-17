1e518ca3a3af7dd8513f2e0e4e13284834188b67
JMMarland <unconfigured@null.spigotmc.org>
2020-07-17 18:25:19 +0100

    Added moderate testing for addition, subtraction, multiplication and division.

--- FormulaGeneratorTests/FormulaGenerationTests.cs
+++ FormulaGeneratorTests/FormulaGenerationTests.cs
@@ -38,6 +38,38 @@ namespace FormulaGeneratorTests
         }
 
         [TestMethod]
+        public void ModerateAdditionTest()
+        {
+            TrainerAndTester trainerAndTester = new TrainerAndTester();
+            Random random = new Random();
+
+            int randValueMin = 0;
+            int randValueMax = 30;
+
+            for (int i = 0; i < 20; i++)
+            {
+                double value1 = random.Next(randValueMin, randValueMax);
+                double value2 = random.Next(randValueMin, randValueMax);
+                double value3 = random.Next(randValueMin, randValueMax);
+                double result = value1 + value2 + value3;
+
+                trainerAndTester.AddInputOutputInstance(value1, value2, value3, result);
+            }
+
+            trainerAndTester.Run();
+
+            Formula formula = trainerAndTester.GetFormula();
+
+            double testVal1 = 45;
+            double testVal2 = 36;
+            double testVal3 = 67;
+            double expected = testVal1 + testVal2 + testVal3;
+            double actual = formula.Run(testVal1, testVal2, testVal3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void SimpleSubtractionTest()
         {
             TrainerAndTester trainerAndTester = new TrainerAndTester();
@@ -63,6 +95,38 @@ namespace FormulaGeneratorTests
             double testVal2 = 2;
             double expected = testVal1 - testVal2;
             double actual = formula.Run(testVal1, testVal2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ModerateSubtractionTest()
+        {
+            TrainerAndTester trainerAndTester = new TrainerAndTester();
+            Random random = new Random();
+
+            int randValueMin = 0;
+            int randValueMax = 30;
+
+            for (int i = 0; i < 20; i++)
+            {
+                double value1 = random.Next(randValueMin, randValueMax);
+                double value2 = random.Next(randValueMin, randValueMax);
+                double value3 = random.Next(randValueMin, randValueMax);
+                double result = value1 - value2 - value3;
+
+                trainerAndTester.AddInputOutputInstance(value1, value2, value3, result);
+            }
+
+            trainerAndTester.Run();
+
+            Formula formula = trainerAndTester.GetFormula();
+
+            double testVal1 = 75;
+            double testVal2 = 32;
+            double testVal3 = 12;
+            double expected = testVal1 - testVal2 - testVal3;
+            double actual = formula.Run(testVal1, testVal2, testVal3);
 
             Assert.AreEqual(expected, actual);
         }
@@ -98,6 +162,38 @@ namespace FormulaGeneratorTests
         }
 
         [TestMethod]
+        public void ModerateMultiplicationTest()
+        {
+            TrainerAndTester trainerAndTester = new TrainerAndTester();
+            Random random = new Random();
+
+            int randValueMin = 0;
+            int randValueMax = 30;
+
+            for (int i = 0; i < 20; i++)
+            {
+                double value1 = random.Next(randValueMin, randValueMax);
+                double value2 = random.Next(randValueMin, randValueMax);
+                double value3 = random.Next(randValueMin, randValueMax);
+                double result = value1 * value2 * value3;
+
+                trainerAndTester.AddInputOutputInstance(value1, value2, value3, result);
+            }
+
+            trainerAndTester.Run();
+
+            Formula formula = trainerAndTester.GetFormula();
+
+            double testVal1 = 67;
+            double testVal2 = 48;
+            double testVal3 = 26;
+            double expected = testVal1 * testVal2 * testVal3;
+            double actual = formula.Run(testVal1, testVal2, testVal3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void SimpleDivisionTest()
         {
             TrainerAndTester trainerAndTester = new TrainerAndTester();
@@ -123,6 +219,38 @@ namespace FormulaGeneratorTests
             double testVal2 = 5;
             double expected = testVal1 / testVal2;
             double actual = formula.Run(testVal1, testVal2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ModerateDivisionTest()
+        {
+            TrainerAndTester trainerAndTester = new TrainerAndTester();
+            Random random = new Random();
+
+            int randValueMin = 0;
+            int randValueMax = 30;
+
+            for (int i = 0; i < 20; i++)
+            {
+                double value1 = random.Next(randValueMin, randValueMax);
+                double value2 = random.Next(randValueMin, randValueMax);
+                double value3 = random.Next(randValueMin, randValueMax);
+                double result = value1 / value2 / value3;
+
+                trainerAndTester.AddInputOutputInstance(value1, value2, value3, result);
+            }
+
+            trainerAndTester.Run();
+
+            Formula formula = trainerAndTester.GetFormula();
+
+            double testVal1 = 89;
+            double testVal2 = 32;
+            double testVal3 = 5;
+            double expected = testVal1 / testVal2 / testVal3;
+            double actual = formula.Run(testVal1, testVal2, testVal3);
 
             Assert.AreEqual(expected, actual);
         }
